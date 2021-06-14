5102fe439016a34919c85a76878488c39273343b
Jai Gaur <jaigaur2609@gmail.com>
2021-06-14 23:52:48 +0530

    add to index.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hey Jai! Let's build the Disney+ Clone!</h1>
+      <h1>Hey Jai! Let's build the DISNEY+ Clone!</h1>
     </div>
   );
 }
