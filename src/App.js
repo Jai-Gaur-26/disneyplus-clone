8bde3b3b42742d054933925bf353dc2dee0002fe
Jai Gaur <jaigaur2609@gmail.com>
2021-06-15 00:39:37 +0530

    create components, login.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Jai! Let's build the DISNEY+ Clone!</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
