ff50ee50ab2792d64a821a8558d44a8732b76d0f
Jai Gaur <jaigaur2609@gmail.com>
2021-06-16 20:50:22 +0530

    add login page background

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,47 @@
 import styled from "styled-components";
 
 const Login = (props) => {
-    return <div>Login</div>;
+    return (
+        <Container>
+            <Content>
+                <BgImage />
+            </Content>
+        </Container>
+    );
 };
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center; /*makes text vertically centered*/
+    align-items: center; /*makes text vertically centered*/
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
+`;
+
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background: url('/images/login-background.jpg');
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+`;
 
 export default Login;
