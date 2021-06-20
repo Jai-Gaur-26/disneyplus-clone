4a6e68131cce1aee9ff5e690f1e7b35490bd9c76
Jai Gaur <jaigaur2609@gmail.com>
2021-06-20 12:19:43 +0530

    Add sign in button

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ const Login = (props) => {
             <Content>
                 <CTA>
                     <CTALogoOne src = '/images/cta-logo-one.svg' alt='' />
+                    <Signup>GET ALL THERE</Signup>
                 </CTA>
                 <BgImage />
             </Content>
@@ -70,6 +71,23 @@ const CTALogoOne = styled.img`
     min-height: 1px;
     display: block;
     width: 100%;
+`;
+
+const Signup = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 6px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
 `;
 
 export default Login;
