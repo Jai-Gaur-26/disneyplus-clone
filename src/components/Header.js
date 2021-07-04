3cf85c9829a5d086e3552c31c3175834f26a4d5a
Jai Gaur <jaigaur2609@gmail.com>
2021-07-04 18:46:06 +0530

    add remaining navBar logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,30 @@ const Header = (props) => {
                     <img src='/images/home-icon.svg' alt="HOME" />
                     <span>HOME</span>
                 </a>
+                <a>
+                    <img src='/images/search-icon.svg' alt="SEARCH" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src='/images/watchlist-icon.svg' alt="WATCHLIST" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src='/images/original-icon.svg' alt="ORIGINALS" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src='/images/movie-icon.svg' alt="MOVIES" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src='/images/series-icon.svg' alt="SERIES" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
         </Nav>
-    )
-}
+    );
+};
 
 const Nav = styled.nav `
     position: fixed;
@@ -66,47 +86,47 @@ const NavMenu = styled.div `
         display: flex;
         align-items: center;
         padding: 0 12px;
-    }
 
-    img {
-        height: 20px;
-        min-width: 20px;
-        width: 20px;
-        z-index: auto;
-    }
-
-    span {
-        color: rgb(249, 249, 249);
-        font-size: 13;
-        letter-spacing: 1.42px;
-        line-height: 1.08;
-        padding: 2px 0px;
-        white-space: nowrap;
-        position: relative;
-
-        &:before {
-            background-color: rgb(249, 249, 249);
-            border-radius: 0px 0px 4px 4px;
-            bottom: -6px;
-            content: "";
-            height: 2px;
-            left: 0px;
-            opacity: 0;
-            position: absolute;
-            right: 0px;
-            transform-origin: left center;
-            transform: scaleX(0);
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-            visibility: hidden;
-            width: auto;
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
         }
-    }
 
-    &:hover {
-        span:before {
-            transform: scaleX(1);
-            visibility: visible;
-            opacity: 1 !important;
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0px;
+            white-space: nowrap;
+            position: relative;
+
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -6px;
+                content: "";
+                height: 2px;
+                left: 0px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+        &:hover {
+            span:before {
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1 !important;
+            }
         }
     }
 `;
