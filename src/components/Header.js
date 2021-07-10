95b65f1c649c318be9a210d4fb855abd0d59a037
Jai Gaur <jaigaur2609@gmail.com>
2021-07-10 22:43:44 +0530

    Add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
     );
 };
@@ -128,6 +129,22 @@ const NavMenu = styled.div `
                 opacity: 1 !important;
             }
         }
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.7px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all .2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
     }
 `;
 
