38b762b355c56e07c9a21820285894c8cffacecc
Jai Gaur <jaigaur2609@gmail.com>
2021-07-12 00:26:58 +0530

    Add user login remembrance code

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+//when the app starts, keep everything empty ""
+const initialState = {
+    name:"",
+    email:"",
+    photo:"",
+}
+
+const userSlice = createSlice({
+    name:'user',
+    initialState, 
+    reducers: {
+        setUserLoginDetails: (state, action) => {
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+            state.photo = action.payload.photo;
+        },
+    },
+});
