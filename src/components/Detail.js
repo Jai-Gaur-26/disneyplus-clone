544c7cc2a8204650559f71f39cd131fd4cb1ec4b
Jai Gaur <jaigaur2609@gmail.com>
2021-07-15 23:44:25 +0530

    Add detail page background image, title

---
+++ src/components/Detail.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+const Detail = (props) => {
+    return (
+        <Container>
+            <Background>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/456A711C19899C881600F6247705E5253EB18C2471D75E5281E1FF6ACB6D2FBA/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
+            </Background>
+
+            <ImageTitle>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4A67A42FB16607DAE7E22266D3F00181965178ED1884047C2D982EE7D89D3554/scale?width=1440&aspectRatio=1.78" alt="" />
+            </ImageTitle>
+        </Container>
+    )
+};
+
+const Container = styled.div`
+    position: relative;
+    min-height: calc(100vh-250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px)
+`;
+
+const Background = styled.div`
+    left: 0px;
+    opacity: 0.8;
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    z-index: -1;
+
+    img {
+        width: 100vw;
+        height: 100vh;
+
+        @media (max-width: 768px) {
+            width: initial;
+        }
+    }
+`;
+
+const ImageTitle = styled.div`
+    align-items: flex-end;
+    display: flex;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+    margin: 0px auto;
+    height: 30vw;
+    min-height: 170px;
+    padding-bottom: 24px;
+    width: 100%;
+
+    img {
+        max-width: 600px;
+        min-width: 200px;
+        width: 35vw;
+    }
+`;
+
+export default Detail;
